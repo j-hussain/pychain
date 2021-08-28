8747d8a981492eda06f0779b8ecd45c3076d9e8e
Jabir Hussain <jabirhussain@protonmail.ch>
2021-08-28 15:06:05 +0100

    Create pychain.py
    docstrings will be added later. barebones for now

---
+++ pychain/pychain.py
@@ -0,0 +1,20 @@
+class Pychain(object):
+
+	def __init__(self) -> None:
+		self.ledger = []
+		self.current_transactions = []
+
+	def add_transaction(self):
+		pass
+
+	def add_block(self):
+		pass
+
+	@staticmethod
+	def hash(block):
+		pass
+
+	@property
+	def final_block(self):
+		pass
+	
