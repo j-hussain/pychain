9e8b15fc819f1666d463b39ad360c3861283dd5c
Jabir Hussain <jabirhussain@protonmail.ch>
2021-09-02 01:02:57 +0100

    Update pychain.py

--- pychain/pychain.py
+++ pychain/pychain.py
@@ -1,14 +1,40 @@
+import hashlib, json
+from time import time
+from tqdm import tqdm as progress_bar
+from typing import Optional
+
 class Pychain(object):
 
 	def __init__(self) -> None:
 		self.ledger = []
 		self.current_transactions = []
 
-	def add_transaction(self):
-		pass
+		# initialise Blockchain
+		self.add_block(previous_hash=1, proof_from_PoW=100)
 
-	def add_block(self):
-		pass
+	def add_transaction(self, sender: str, recipient: str, amount: int) -> int:
+		self.current_transactions.append({
+			"from": sender,
+			"to": recipient,
+			"amount" : amount
+			})
+
+		return self.final_block["index"] + 1
+
+	def add_block(self, proof_from_PoW: int, previous_hash: Optional[str] = None) -> dict:
+		block = {
+		"transactions" : self.current_transactions,
+		"proof" : proof_from_PoW,
+		"index" : len(self.chain) + 1,
+		"timestamp" : time(),
+		"previous_hash" : self.hash(self.ledger[-1]) or previous_hash
+		}
+
+		# reset current transactions
+		self.current_transactions = []
+
+		self.ledger.append(block)
+		return block
 
 	@staticmethod
 	def hash(block):
@@ -17,4 +43,5 @@ class Pychain(object):
 	@property
 	def final_block(self):
 		pass
+	
 	
